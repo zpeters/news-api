359f79bd161d67c6a2c13ed412fe3494d6e2f286
Ryan Angelo Cadag <ryan@pocketmarket.com.ph>
2018-02-15 11:12:06 +0800

    Initial Commit

---
+++ lib/news_api.ex
@@ -0,0 +1,18 @@
+defmodule NewsApi do
+  @moduledoc """
+  Documentation for NewsApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NewsApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
