69dfccf6aa5f5c24c7e50700dd0ed12064883290
Ryan Angelo Cadag <ryan@pocketmarket.com.ph>
2018-02-15 14:24:21 +0800

    operation for getting news

--- lib/news_api.ex
+++ lib/news_api.ex
@@ -1,18 +1,45 @@
 defmodule NewsApi do
   @moduledoc """
-  Documentation for NewsApi.
+  Module for news api operations
   """
 
-  @doc """
-  Hello world.
+  @base_url "https://newsapi.org/v2/"
+  
+  @spec get(String.t()) :: map()
+  def get(country) do
+    result = call_api("#{@base_url}top-headlines?country=#{country}", headers())
+    match_result(result)
+  end
+  
+  @spec get(String.t(), String.t()) :: map()
+  def get(country, category) do
+    result = call_api("#{@base_url}top-headlines?country=#{country}&category=#{category}", headers())
+    match_result(result)
+  end
+  
+  @spec headers :: list()
+  defp headers do
+    api_key = Application.get_env(:news_api, :api_key)
+    ["Authorization": "Bearer #{api_key}", "Accept": "Application/json"]
+  end
 
-  ## Examples
+  @spec call_api(String.t(), list) :: String.t()
+  defp call_api(url, headers) do
+    HTTPoison.get!(url, headers) 
+  end
+  
+  @spec match_result(struct()) :: map()
+  defp match_result(result) do
+    case result do
+      %{status_code: 200, body: body} ->
+        decode_result(body)
+      %{status_code: 401} ->
+        %{message: "API key is missing"}
+    end
+  end
 
-      iex> NewsApi.hello
-      :world
-
-  """
-  def hello do
-    :world
+  @spec decode_result(String.t()) :: map()
+  defp decode_result(result) do
+    Poison.decode!(result)
   end
 end
