b6eba825f889523b7189a024a2c850d08fcd2064
Ryan Angelo Cadag <ryan@pocketmarket.com.ph>
2018-02-15 11:16:25 +0800

    add project description

--- mix.exs
+++ mix.exs
@@ -6,23 +6,30 @@ defmodule NewsApi.Mixfile do
       app: :news_api,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: "Wrapper for News API",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+  
+  defp package do
+    [
+      maintainers: ["radvc"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/radvc/news-api"}
     ]
   end
 end
